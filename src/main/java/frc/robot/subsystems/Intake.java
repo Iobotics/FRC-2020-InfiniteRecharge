5d8ee55f5dbed2cf6b6c36699b808387a005d943
Mallhew <mah2001@iolani.org>
2020-02-26 15:28:33 -1000

    Add basic intake subsystem

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,35 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.RobotMap;
+
+public class Intake extends SubsystemBase {
+  /**
+   * Creates a new Intake.
+   */
+
+  private TalonSRX intake;
+
+  public Intake() {
+    intake = new TalonSRX(RobotMap.kIntake);
+  }
+
+  public void setPercent(double demand) {
+    intake.set(ControlMode.PercentOutput, demand);
+  } 
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
